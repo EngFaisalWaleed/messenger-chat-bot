8936f2f3af5f810799c33b89096b8a1f793cd3d5
Usman Asif <usmanasif@users.noreply.github.com>
2022-01-19 18:49:03 -0800

    Initial setup for the project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(morgan('dev')); // log every request to the console.
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json()); 
+
+// app routes
+require('./routes/webhookVerify')(app);
+
+app.listen(app.get('port'), function() {
+  const url = 'http://localhost:' + app.set('port');
+  console.log('Application running on port: ', app.get('port'));
+});
